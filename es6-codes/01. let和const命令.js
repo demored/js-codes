2c3a56770b2a6086a89b7c05598cdfe701ee0a76
jueying <jueying@163.com>
2020-09-30 14:38:28 +0800

    update

--- es6-codes/01. let和const命令.js
+++ es6-codes/01. let和const命令.js
@@ -1,10 +1,21 @@
-let abc = "hello"
-{let abc = "world"}
-if(true){
-    function hello(){
-        console.log("test")
-    }
-}
+// let abc = "hello"
+// {let abc = "world"}
+// if(true){
+//     function hello(){
+//         console.log("test")
+//     }
+// }
 
+
+// console.log(b);
+// var b = 111;
+
+var pi = 3.2;
+console.log(pi);
+
+if(true){
+    const pi = 1;
+    console.log(pi);
+}
 
 
